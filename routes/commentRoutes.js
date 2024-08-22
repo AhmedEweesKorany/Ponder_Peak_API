dade2659610dbd050d39669deb1e2abc3fc8e067
Ahmed Ewees Korany <ahmedewaisvi34@gmail.com>
2024-08-22 09:12:00 +0300

    add delete routes  for delete comment

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -8,6 +8,6 @@ router.get('/:id',commentController.getPostComment)
 // router.get("/:slug",commentController.getPostBySlug)
 router.post('/create',authGurad,commentController.createComment)
 // router.put('/update/:slug',authGurad,commentController.updatePost)
-// router.delete('/delete/:slug',authGurad,commentController.deletePost)
+router.delete('/delete/:id',authGurad,commentController.deleteComment)
 
 module.exports = router  
