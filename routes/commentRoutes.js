a3a3b060beb3a66c16e8d126eac5246e647cc1fb
Ahmed Ewees Korany <ahmedewaisvi34@gmail.com>
2024-08-22 10:07:39 +0300

    add update comment endpoint

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -7,7 +7,7 @@ const authGurad = require("../middlewares/authMiddleware")
 router.get('/:id',commentController.getPostComment)
 // router.get("/:slug",commentController.getPostBySlug)
 router.post('/create',authGurad,commentController.createComment)
-// router.put('/update/:slug',authGurad,commentController.updatePost)
+router.put('/update/:id',authGurad,commentController.updateComment)
 router.delete('/delete/:id',authGurad,commentController.deleteComment)
 
 module.exports = router  
