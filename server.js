22f06d0e278f9907cc68f9f76a2dcb4623515b65
Ahmed Ewees Korany <ahmedewaisvi34@gmail.com>
2024-08-29 08:25:36 +0300

    update cors policy

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ const app = express();
 
 const Port = process.env.PORT || 5000
 // Middleware to parse JSON request bodies
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 200,
+}))
 app.use(express.json());
 //mange different routes 
 app.use("/api/users",require("./routes/userRoutes"))
