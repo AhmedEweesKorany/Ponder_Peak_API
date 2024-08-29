47cea56ff8673665f4f94bf136840537b19b5d6c
Ahmed Ewees Korany <ahmedewaisvi34@gmail.com>
2024-08-29 09:24:21 +0300

    update server.js

--- server.js
+++ server.js
@@ -13,13 +13,7 @@ const app = express();
 
 const Port = process.env.PORT || 5000
 // Middleware to parse JSON request bodies
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    optionsSuccessStatus: 200,
-}))
+app.use(cors())
 
 app.use(express.json());
 // main page 
